0ac40be0688de86640781df308e42c99e7a49202
ICoelloC <ivancoellocortes@gmail.com>
2022-05-19 22:04:40 +0200

    Funcionalidad del nav drawer, permite desplazarse entre los distintos fragmente de la aplicación.

--- app/src/main/java/com/icoelloc/renter/main/MainActivity.kt
+++ app/src/main/java/com/icoelloc/renter/main/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.icoelloc.renter.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
 import com.icoelloc.renter.R
+import com.icoelloc.renter.screens.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,11 +31,11 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.nav_mi_domicilio -> Toast.makeText(applicationContext,"clicked my home",Toast.LENGTH_SHORT).show()
-                R.id.nav_mis_propiedades -> Toast.makeText(applicationContext,"clicked my properties",Toast.LENGTH_SHORT).show()
-                R.id.nav_cerca_mi -> Toast.makeText(applicationContext,"clicked close to me",Toast.LENGTH_SHORT).show()
-                R.id.nav_buscar -> Toast.makeText(applicationContext,"clicked search",Toast.LENGTH_SHORT).show()
-                R.id.nav_mi_perfil -> Toast.makeText(applicationContext,"clicked my account",Toast.LENGTH_SHORT).show()
+                R.id.nav_mi_domicilio -> replaceFragment(MyHomeFragment())
+                R.id.nav_mis_propiedades -> replaceFragment(MyPropertiesFragment())
+                R.id.nav_cerca_mi -> replaceFragment(CloseToMeFragment())
+                R.id.nav_buscar -> replaceFragment(SearchFragment())
+                R.id.nav_mi_perfil -> replaceFragment(MyAccountFragment())
             }
             true
         }
@@ -47,4 +50,13 @@ class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+
+    private fun replaceFragment(fragment:Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
